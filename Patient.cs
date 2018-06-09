5262c341caea178bb611b0a41d0b326839ce4a76
Dimtrivh <39410964+Dimtrivh@users.noreply.github.com>
2018-06-09 08:38:12 +0200

    Patient model

---
+++ Patient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Patientendossier.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; } 
+        public string FirstName { get; set; }
+        public int BirthYear { get; set; }
+
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Patient> Patienten { get; set; }
+    }
+
+
+    public class dossier 
+    {
+        public int DossierNr { get; set; }
+
+
+    }
+
+
+}               
